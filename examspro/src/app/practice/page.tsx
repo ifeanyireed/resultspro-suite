cccd6765295f2dc1af4a905a789d429427795956
Ifeanyi Ibeh <reedbreeddigital@gmail.com>
2026-09-08 22:03:23 +0100

    UI: Elevate Exam popup z-index above navbar

--- examspro/src/app/practice/page.tsx
+++ examspro/src/app/practice/page.tsx
@@ -211,7 +211,7 @@ export default function PracticePage() {
                     {cat.exams.map((exam, examIdx) => (
                       <div
                         key={exam.id}
-                        className="relative"
+                        className={`relative ${hoveredExamId === exam.id ? "z-[200]" : "z-10"}`}
                         onMouseEnter={() => handleMouseEnter(exam.id)}
                         onMouseLeave={handleMouseLeaveFinal}
                       >
