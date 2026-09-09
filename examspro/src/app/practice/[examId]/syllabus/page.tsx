9cb472c880b27fa69bc765d4728249c923421469
Ifeanyi Ibeh <reedbreeddigital@gmail.com>
2026-09-09 09:32:08 +0100

    Refactor SyllabusClient server component nested UI to light mode and convert green study buttons to brand gradients

--- examspro/src/app/practice/[examId]/syllabus/page.tsx
+++ examspro/src/app/practice/[examId]/syllabus/page.tsx
@@ -29,7 +29,7 @@ export default async function ExamSyllabusPage(props: { params: Promise<{ examId
   const syllabus = await getSyllabus(examId);
 
   return (
-    <div className="min-h-screen bg-navy flex flex-col">
+    <div className="min-h-screen bg-slate-50 flex flex-col">
       <Navbar />
       <SyllabusClient syllabus={syllabus} examId={examId} />
       <Footer />
