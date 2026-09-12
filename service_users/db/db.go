8efdb3393f7aa075de85418719bcd69554b74fb3
Ifeanyi Ibeh <reedbreeddigital@gmail.com>
2026-09-13 00:23:10 +0100

    fix(service_users): add db wrapper to automatically translate mysql placeholders to postgres format

--- service_users/db/db.go
+++ service_users/db/db.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"log"
 	"time"
+	"fmt"
 
 	_ "github.com/lib/pq"
 	"gorm.io/driver/postgres"
@@ -11,33 +12,65 @@ import (
 	"gorm.io/gorm/logger"
 )
 
+type DBWrapper struct {
+	*sql.DB
+}
+
+// Rebind translates ? to $1, $2 etc for postgres
+func Rebind(query string) string {
+	qb := []byte(query)
+	var out []byte
+	j := 1
+	for _, b := range qb {
+		if b == '?' {
+			out = append(out, []byte(fmt.Sprintf("$%d", j))...)
+			j++
+		} else {
+			out = append(out, b)
+		}
+	}
+	return string(out)
+}
+
+func (w *DBWrapper) QueryRow(query string, args ...interface{}) *sql.Row {
+	return w.DB.QueryRow(Rebind(query), args...)
+}
+
+func (w *DBWrapper) Query(query string, args ...interface{}) (*sql.Rows, error) {
+	return w.DB.Query(Rebind(query), args...)
+}
+
+func (w *DBWrapper) Exec(query string, args ...interface{}) (sql.Result, error) {
+	return w.DB.Exec(Rebind(query), args...)
+}
+
 var (
-	DB     *sql.DB
+	DB     *DBWrapper
 	GormDB *gorm.DB
 )
 
 func InitDB(dataSourceName string) {
 	var err error
 
-	DB, err = sql.Open("postgres", dataSourceName)
+	rawDB, err := sql.Open("postgres", dataSourceName)
 	if err != nil {
 		log.Fatalf("Failed to open database: %v", err)
 	}
 
-	// Optimize connection pooling for shared hosting limits
-	// Hostinger max_connections_per_hour is very strict (e.g., 500)
-	DB.SetMaxOpenConns(5)
-	DB.SetMaxIdleConns(5)
-	DB.SetConnMaxLifetime(time.Hour)
+	rawDB.SetMaxOpenConns(5)
+	rawDB.SetMaxIdleConns(5)
+	rawDB.SetConnMaxLifetime(time.Hour)
 
-	if err = DB.Ping(); err != nil {
+	if err = rawDB.Ping(); err != nil {
 		log.Printf("Note: Postgres ping timeout: %v", err)
 	} else {
 		log.Println("Microservice connected to Postgres successfully")
 	}
 
+	DB = &DBWrapper{DB: rawDB}
+
 	GormDB, err = gorm.Open(postgres.New(postgres.Config{
-		Conn: DB,
+		Conn: rawDB,
 	}), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Warn),
 	})
