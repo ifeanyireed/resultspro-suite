b31665efeccc95dfaf3134d5047989159bd445a2
Ifeanyi Ibeh <reedbreeddigital@gmail.com>
2026-09-13 01:06:04 +0100

    fix(service_users): rewrite HandleResolveTenant with gorm to fix postgres placeholder crash

--- service_users/handlers/tenant.go
+++ service_users/handlers/tenant.go
@@ -667,37 +667,10 @@ func HandleResolveTenant(w http.ResponseWriter, r *http.Request) {
 	}
 
 	var t models.Tenant
-	var tenantCode, shortName, motto, logoURL, logoEmoji, primaryColor, secondaryColor, accentColor sql.NullString
-	var tenantType, enabledModules, settings sql.NullString
-
-	query := `
-		SELECT id, type, name, slug, default_subdomain, custom_domain, tenant_code, short_name, motto, logo_url, logo_emoji, primary_color, secondary_color, accent_color, enabled_modules, settings 
-		FROM tenants WHERE default_subdomain = ? OR custom_domain = ?`
-
-	err := db.DB.QueryRow(query, domain, domain).Scan(
-		&t.ID, &tenantType, &t.Name, &t.Slug, &t.DefaultSubdomain, &t.CustomDomain, &tenantCode, &shortName, &motto, &logoURL, &logoEmoji, &primaryColor, &secondaryColor, &accentColor,
-		&enabledModules, &settings,
-	)
-
-	if err == sql.ErrNoRows {
+	if err := db.GormDB.Where("default_subdomain = ? OR custom_domain = ?", domain, domain).First(&t).Error; err != nil {
 		utils.JSONError(w, http.StatusNotFound, "Tenant not found")
 		return
-	} else if err != nil {
-		utils.JSONError(w, http.StatusInternalServerError, "Database error")
-		return
 	}
-
-	if tenantCode.Valid { t.TenantCode = tenantCode.String }
-	if tenantType.Valid { t.Type = tenantType.String }
-	if shortName.Valid { t.ShortName = shortName.String }
-	if motto.Valid { t.Motto = motto.String }
-	if logoURL.Valid { t.LogoURL = logoURL.String }
-	if logoEmoji.Valid { t.LogoEmoji = logoEmoji.String }
-	if primaryColor.Valid { t.PrimaryColor = primaryColor.String }
-	if secondaryColor.Valid { t.SecondaryColor = secondaryColor.String }
-	if accentColor.Valid { t.AccentColor = accentColor.String }
-	if enabledModules.Valid { t.EnabledModules = enabledModules.String }
-	if settings.Valid { t.Settings = settings.String }
 
 	utils.JSONResponse(w, http.StatusOK, map[string]interface{}{
 		"tenant": t,
