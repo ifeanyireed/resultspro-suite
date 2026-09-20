539c56149bccb50dce933237db93272f5af59127
Ifeanyi Ibeh <reedbreeddigital@gmail.com>
2026-09-20 10:55:41 +0100

    chore: remove test files

--- test_drive_url.js
+++
@@ -1,8 +0,0 @@
-const url = "https://drive.google.com/file/d/11Tx0jYXKRHbgL0KbSMrBofL91Lp3vhqN/view?usp=sharing";
-const formatDriveUrl = (url) => {
-  if (url.includes('drive.google.com/file/d/')) {
-    return url.replace(/\/view.*/, '/preview');
-  }
-  return url;
-}
-console.log(formatDriveUrl(url));
